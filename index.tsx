45a12ed6ad7ffd6a4a89e843959d2c9b42959f8d
flashhuang <huang19990915@163.com>
2022-09-25 12:53:40 +0800

    commit

---
+++ index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "antd/dist/antd.min.css";
+import "./i18n/configs";
+import { Provider } from "react-redux";
+import rootStore from "./redux/store";
+import axios from "axios";
+import { PersistGate } from "redux-persist/integration/react";
+
+axios.defaults.headers['x-icode'] = '72D23345F805F1ED';
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Provider store={rootStore.store}>
+      <PersistGate persistor={rootStore.persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
